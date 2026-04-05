0d5d8a6a1f7a19e75dcb29a0e165227285768a4e
maximiliansiege <autonomy@tuta.com>
2026-04-05 06:19:30 +0200

    Add GitHub, Twitter, and Telegram links

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -5,6 +5,11 @@ function App() {
     <div className="container">
       <h1>Pokelend</h1>
       <p>Coming Soon</p>
+      <div className="links">
+        <a href="https://github.com/pokelend-xyz" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://x.com/pokelend_xyz" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <a href="https://t.me/pokelend_xyz" target="_blank" rel="noopener noreferrer">Telegram</a>
+      </div>
     </div>
   )
 }
